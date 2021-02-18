6328bcb75b913ce546a89e58c0db5fe68375a885
HerbertSousa <herberti1405@outlook.com>
2021-02-18 15:43:04 -0300

    feature: create component Taost

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import React from 'react';
 
 import SignIn from './pages/SignIn';
 import GlobalStyle from './styles/global';
+
+import ToastContainer from './components/ToastContainer';
 import { AuthProvider } from './hooks/AuthContext';
 
 const App: React.FC = () => (
@@ -10,6 +12,7 @@ const App: React.FC = () => (
     <AuthProvider>
       <SignIn />
     </AuthProvider>
+    <ToastContainer />
     <GlobalStyle />
   </>
 );
